a6dd5512708e2a55559aa46e9e5ebea65ff78121
InSal99 <85535998+InSal99@users.noreply.github.com>
2025-02-03 00:35:15 +0700

    third commit

---
+++ app/src/main/java/com/example/uxassignment1/SpacingItemDecoration.kt
@@ -0,0 +1,31 @@
+package com.example.uxassignment1
+
+import android.content.Context
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class StartSpacingItemDecoration(context: Context, private val spaceInPx: Int) : RecyclerView.ItemDecoration() {
+
+    private val space: Int
+
+    init {
+        val density = context.resources.displayMetrics.density
+        space = (spaceInPx * density).toInt()
+    }
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        // Add space only at the start (left/top)
+        if (parent.getChildAdapterPosition(view) == 0) {
+            if (parent.layoutManager is LinearLayoutManager) {
+                val layoutManager = parent.layoutManager as LinearLayoutManager
+                if (layoutManager.orientation == RecyclerView.HORIZONTAL) {
+                    outRect.left = space // Add space on the left for horizontal orientation
+                } else {
+                    outRect.top = space // Add space at the top for vertical orientation
+                }
+            }
+        }
+    }
+}
